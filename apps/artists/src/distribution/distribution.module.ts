6175cfb032a6fe8f714e75e6859ecb41c2585941
Azutech <azunna.onugha@gmail.com>
2025-11-07 09:54:20 +0100

    add_feat ()  Token service created for distro

--- apps/artists/src/distribution/distribution.module.ts
+++ apps/artists/src/distribution/distribution.module.ts
@@ -3,9 +3,10 @@ import { DistributionService } from './distribution.service';
 import { DistributionController } from './distribution.controller';
 import { DistributionRepository } from './repository/distribution.repository';
 import { PrismaService } from 'prisma/prisma.service';
+import { TokenRepository } from './repository/token.repository';
 
 @Module({
   controllers: [DistributionController],
-  providers: [DistributionService, PrismaService, DistributionRepository],
+  providers: [DistributionService, PrismaService, DistributionRepository, TokenRepository],
 })
 export class DistributionModule {}
